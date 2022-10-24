2a6a231f643aeab9bb2aab7513aad792e2324186
DESKTOP-SOAMH3H\Syihab <syehab94@gmail.com>
2022-10-24 13:55:40 +0700

    Sub test unit test

--- helper/helloworld_test.go
+++ helper/helloworld_test.go
@@ -47,3 +47,25 @@ func TestSkip(t *testing.T) {
 	result := HelloWorld("Syihab")
 	require.Equal(t, "Hello Syihab", result, "Result must be 'Hello Syihab'")
 }
+
+func TestMain(m *testing.M) {
+	//before
+	fmt.Println("Before unit test")
+
+	m.Run()
+
+	//after
+	fmt.Println("After unit test")
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Syihab", func(t *testing.T){
+		result := HelloWorld("Syihab")
+		require.Equal(t, "Hello Syihab", result, "Result must be 'Hello Syihab'")
+	})
+
+	t.Run("Aru", func(t *testing.T){
+		result := HelloWorld("Aru")
+		require.Equal(t, "Hi Aru", result, "Result must be 'Hello Aru'")
+	})
+}
