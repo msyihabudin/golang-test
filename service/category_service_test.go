9b2e3af44f6f9d18166774e155ef675c8d199afe
DESKTOP-SOAMH3H\Syihab <syehab94@gmail.com>
2022-10-24 14:56:04 +0700

    Add success unit test case

--- service/category_service_test.go
+++ service/category_service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"testing"
+	"unittest/entity"
 	"unittest/repository"
 
 	"github.com/stretchr/testify/assert"
@@ -11,11 +12,26 @@ import (
 var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
 var categoryService = CategoryService{Repository: categoryRepository}
 
-func TestCategoryService_Get(t *testing.T) {
+func TestCategoryService_GetNotFound(t *testing.T) {
 	//mock
 	categoryRepository.Mock.On("FindById", "1").Return(nil)
 
 	category, err := categoryService.Get("1")
 	assert.Nil(t, category)
 	assert.NotNil(t, err)
+}
+
+func TestCategoryService_GetSuccess(t *testing.T) {
+	category := entity.Category{
+		Id:   "1",
+		Name: "Syihab",
+	}
+	//mock
+	categoryRepository.Mock.On("FindById", "2").Return(category)
+
+	result, err := categoryService.Get("2")
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, category.Id, result.Id)
+	assert.Equal(t, category.Name, result.Name)
 }
